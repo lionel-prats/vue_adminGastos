3f68eb304263c989753f76cf6aab52ec567081a9
lionel-prats <lionel.prats.c@gmail.com>
2024-07-28 21:51:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import Filtro from "./components/Filtro.vue"
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Planificador de gastos</h1>
+      <Filtro />
+    </header>
+  </div>
+</template>
+
+<style scoped>
+  h1, h2 {
+    color: red;
+  }
+</style>
